2236090c82655978997357e8e4dd3ab5eec07a39
Alberto <info@peralstudio.com>
2021-12-01 00:58:44 +0100

    ✨ Add AnimatedLoad component

---
+++ src/components/AnimatedLoad.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { AdMobInterstitial } from "expo-ads-admob";
+
+import AnimatedLoader from "react-native-animated-loader";
+import { styles } from "../styles/AnimatedLoadStyles";
+
+export default function AnimatedLoad() {
+    const [visible, setVisible] = useState(true);
+    const [buttonDisable, setButtonDisable] = useState(true);
+    useEffect(() => {
+        setInterval(() => {
+            setButtonDisable(false);
+        }, 2500);
+
+        const chargeAd = async () => {
+            await AdMobInterstitial.setAdUnitID(
+                "ca-app-pub-3940256099942544/1033173712"
+            ); // Test ID, Replace with your-admob-unit-id
+            await AdMobInterstitial.requestAdAsync({
+                servePersonalizedAds: true,
+            });
+        };
+        chargeAd();
+    }, []);
+
+    const showAd = async () => {
+        await AdMobInterstitial.showAdAsync();
+    };
+
+    return (
+        <>
+            <AnimatedLoader
+                visible={visible}
+                overlayColor="#17181f"
+                animationStyle={{
+                    width: buttonDisable ? 100 : 0,
+                    height: buttonDisable ? 100 : 0,
+                }}
+                speed={1}
+            >
+                <View style={styles.container}>
+                    <Text style={styles.text}>
+                        {buttonDisable ? "Cargando Tareas..." : ""}
+                    </Text>
+                    {!buttonDisable && (
+                        <View>
+                            <Icon
+                                name="check-circle"
+                                size={80}
+                                color="green"
+                                style={styles.icon}
+                            />
+                            <Text style={styles.text}>Tareas Cargadas</Text>
+                        </View>
+                    )}
+                    <Button
+                        title="ENTRAR"
+                        onPress={() => {
+                            showAd();
+                            setVisible(false);
+                        }}
+                        disabled={buttonDisable}
+                        buttonStyle={{
+                            backgroundColor: "#b738fe",
+                        }}
+                    />
+                </View>
+            </AnimatedLoader>
+        </>
+    );
+}
---
+++ src/styles/AnimatedLoadStyles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        marginBottom: 100,
+    },
+    textButton: {
+        color: "#fff",
+        textTransform: "uppercase",
+    },
+    text: {
+        color: "white",
+        fontSize: 20,
+        textAlign: "center",
+        marginBottom: 30,
+    },
+    icon: {
+        marginBottom: 10,
+        textAlign: "center",
+    },
+});
