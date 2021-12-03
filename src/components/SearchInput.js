a144d71e6783156242f41aede071606c62a7fe20
Alberto <info@peralstudio.com>
2021-12-03 01:07:12 +0100

    Update SearchInput.js

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,9 +3,17 @@ import { View, TextInput } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { styles } from "../styles/SearchInputStyles";
 
+import { AdMobBanner } from "expo-ads-admob";
+
 const SearchInput = ({ valueSearch, onChangeTextSearch }) => {
     return (
         <View style={styles.inputSection}>
+            <AdMobBanner
+                bannerSize="banner"
+                adUnitID="ca-app-pub-6203383529182342/1753513956"
+                servePersonalizedAds={true}
+                onDidFailToReceiveAdWithError={(error) => console.log(error)}
+            />
             <View style={styles.inputContainer}>
                 <View style={styles.iconStyle}>
                     <AntDesign name="search1" size={16} color="#858585" />
