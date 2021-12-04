10ccce32407853ebf10b5824f7664521bd108328
Alberto <info@peralstudio.com>
2021-12-04 18:07:56 +0100

    ✨ Add total /status of task

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,9 +3,12 @@ import { View, Text, TouchableOpacity, Image, StatusBar } from "react-native";
 import firebase, { auth } from "../services/firebaseConfig";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { styles } from "../styles/ProfileStyles";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Profile({ navigation }) {
+export default function Profile({ task }) {
     const [loggedUser, setLoggedUser] = useState(null);
+
+    const navigation = useNavigation();
 
     firebase.auth().onAuthStateChanged((firebaseUser) => {
         setLoggedUser(firebaseUser);
@@ -16,6 +19,11 @@ export default function Profile({ navigation }) {
             navigation.navigate("Login");
         });
     };
+
+    const taskPending = task.filter((task) => task.status === "Pendiente");
+    const taskInProgress = task.filter((task) => task.status === "En Curso");
+    const taskFinished = task.filter((task) => task.status === "Finalizada");
+    const taskCanceled = task.filter((task) => task.status === "Cancelada");
 
     return (
         <View style={styles.container}>
@@ -44,6 +52,32 @@ export default function Profile({ navigation }) {
             >
                 <Text style={styles.buttonTitle}>CERRAR SESIÓN</Text>
             </TouchableOpacity>
+            <View style={styles.footer}>
+                <Text style={{ color: "white", fontSize: 18 }}>
+                    {taskPending.length}{" "}
+                    {taskPending.length === 1
+                        ? "tarea pendiente"
+                        : "tareas pendientes"}
+                </Text>
+                <Text style={{ color: "white", fontSize: 18 }}>
+                    {taskInProgress.length}{" "}
+                    {taskInProgress.length === 1
+                        ? "tarea en curso"
+                        : "tareas en curso"}
+                </Text>
+                <Text style={{ color: "white", fontSize: 18 }}>
+                    {taskFinished.length}{" "}
+                    {taskFinished.length === 1
+                        ? "tarea finalizada"
+                        : "tareas finalizadas"}
+                </Text>
+                <Text style={{ color: "white", fontSize: 18 }}>
+                    {taskCanceled.length}{" "}
+                    {taskCanceled.length === 1
+                        ? "tarea cancelada"
+                        : "tareas canceladas"}
+                </Text>
+            </View>
         </View>
     );
 }
